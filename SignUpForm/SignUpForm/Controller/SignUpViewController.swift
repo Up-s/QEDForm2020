5f05d666cce11e0640e96c5aad2fe08026c5caaa
dldbdjq <dldbdjq@gmail.com>
2020-01-12 01:20:37 +0900

    UPDATE: SignUpFormView Animate Success
     - 애니매이션 완료 생각보다 시간은 오래 걸렸음

--- SignUpForm/SignUpForm/Controller/SignUpViewController.swift
+++ SignUpForm/SignUpForm/Controller/SignUpViewController.swift
@@ -8,12 +8,19 @@
 
 import UIKit
 
+private struct Standard {
+  static let space: CGFloat = 8
+  
+  static let scrollViewInset: CGFloat = 16
+}
+
 class SignUpViewController: UIViewController {
   
-  private var index = 0 { didSet { print(index) } }
-  private let signUpForms = ["000", "111", "222", "333", "444", "555", "666", "777", "888", "999"]
+  private var visibleIndex = 0
+  private var formViewSpace: CGFloat = 0
+  private var signUpForms = ["000", "111", "222", "333", "444", "555", "666", "777", "888", "999"]
   private var visibleForms = [SignUpFormView]()
-  private var tempContraints = [NSLayoutConstraint]()
+  private var topConstraints = [NSLayoutConstraint]()
   
   private let mainScrollView = UIScrollView()
   
@@ -23,17 +30,46 @@ class SignUpViewController: UIViewController {
     
     navigation()
     mainScrollViewSet()
-    firstFormSet()
-//    formViewSet()
+    setFormView()
+  }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    formViewSpace = visibleForms[0].frame.maxY
+    print(formViewSpace)
   }
   
   private func navigation() {
     navigationItem.title = "Sign Up"
     navigationController?.navigationBar.shadowImage = UIImage()
     navigationController?.navigationBar.barTintColor = .white
+    
+    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "✚", style: .done, target: self, action: #selector(formViewUpdateAnimate))
   }
   
-  
+  @objc private func formViewUpdateAnimate() {
+    visibleIndex += 1
+    
+    guard signUpForms.count > visibleIndex else { return }
+    
+    UIView.animate(withDuration: 0.2) { [weak self] in
+      guard let `self` = self else { return }
+      for i in 0...(self.visibleIndex - 1) {
+        self.topConstraints[i].constant += self.formViewSpace
+      }
+      self.view.layoutIfNeeded()
+    }
+    
+    UIView.animate(
+      withDuration: 0.2,
+      delay: 0.1,
+      options: [],
+      animations: { [weak self] in
+        guard let `self` = self else { return }
+        self.visibleForms[self.visibleIndex].alpha = 1
+        self.view.layoutIfNeeded()
+    })
+  }
   
   private func mainScrollViewSet() {
     mainScrollView.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
@@ -45,97 +81,34 @@ class SignUpViewController: UIViewController {
     mainScrollView.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
     mainScrollView.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
     mainScrollView.bottomAnchor.constraint(equalTo: guide.bottomAnchor).isActive = true
-    
   }
   
-  private func createformView() {
-    let tempSignUpFormView = SignUpFormView(title: signUpForms[index])
-    visibleForms.insert(tempSignUpFormView, at: 0)
-    mainScrollView.addSubview(tempSignUpFormView)
-    tempSignUpFormView.textField.becomeFirstResponder()
-    tempSignUpFormView.textField.delegate = self
-    tempSignUpFormView.translatesAutoresizingMaskIntoConstraints = false
-    tempSignUpFormView.centerXAnchor.constraint(equalTo: mainScrollView.centerXAnchor).isActive = true
-    tempSignUpFormView.widthAnchor.constraint(equalTo: mainScrollView.widthAnchor, constant: (-Standard.scrollViewInset * 2)).isActive = true
-    
-    index += 1
-  }
-  
-  private func firstFormSet() {
-    createformView()
-    
-    tempContraints.append(visibleForms[0].topAnchor.constraint(equalTo: mainScrollView.topAnchor, constant: Standard.scrollViewInset))
-    tempContraints.append(visibleForms[0].bottomAnchor.constraint(equalTo: mainScrollView.bottomAnchor, constant: -Standard.scrollViewInset))
-    
-    tempContraints.forEach { $0.isActive = true }
-  }
-  
-  private func formUpdate() {
-    tempContraints.forEach { $0.isActive = false }
-    tempContraints.removeAll()
-    
-    createformView()
-    
-    for i in 0..<visibleForms.count {
-      switch i {
-      case 0:
-        tempContraints.append(visibleForms[i].topAnchor.constraint(equalTo: mainScrollView.topAnchor, constant: Standard.scrollViewInset))
-        
-      case visibleForms.count - 1:
-        tempContraints.append(visibleForms[i].bottomAnchor.constraint(equalTo: mainScrollView.bottomAnchor, constant: -Standard.scrollViewInset))
-        fallthrough
-        
-      default:
-        tempContraints.append(visibleForms[i].topAnchor.constraint(equalTo: visibleForms[i - 1].bottomAnchor, constant: Standard.scrollViewInset))
-      }
-    }
-    
-    tempContraints.forEach { $0.isActive = true }
-//    view.updateConstraints()
-//    view.layoutIfNeeded()
-  }
-  
-  
-  
-  
-  
-  private func formViewSet() {
-    signUpForms.forEach {
-      let tempSignUpFormView = SignUpFormView(title: $0)
+  private func setFormView() {
+    signUpForms.enumerated().forEach {
+      let tempSignUpFormView = SignUpFormView(title: $1)
       visibleForms.append(tempSignUpFormView)
       mainScrollView.addSubview(tempSignUpFormView)
       tempSignUpFormView.translatesAutoresizingMaskIntoConstraints = false
       tempSignUpFormView.centerXAnchor.constraint(equalTo: mainScrollView.centerXAnchor).isActive = true
+      let tempTopConstraint = tempSignUpFormView.topAnchor.constraint(equalTo: mainScrollView.topAnchor, constant: Standard.scrollViewInset)
+      tempTopConstraint.isActive = true
+      topConstraints.append(tempTopConstraint)
+      
       tempSignUpFormView.widthAnchor.constraint(equalTo: mainScrollView.widthAnchor, constant: (-Standard.scrollViewInset * 2)).isActive = true
-    }
-    
-    visibleForms.reverse()
-    
-    for i in 0..<visibleForms.count {
-      switch i {
-      case 0:
-        visibleForms[i].topAnchor.constraint(equalTo: mainScrollView.topAnchor, constant: Standard.scrollViewInset).isActive = true
-        
-      case visibleForms.count - 1:
-        visibleForms[i].bottomAnchor.constraint(equalTo: mainScrollView.bottomAnchor, constant: -Standard.scrollViewInset).isActive = true
-        fallthrough
-        
-      default:
-        visibleForms[i].topAnchor.constraint(equalTo: visibleForms[i - 1].bottomAnchor, constant: Standard.scrollViewInset).isActive = true
+      
+      if $0 == 0 {
+        tempSignUpFormView.bottomAnchor.constraint(equalTo: mainScrollView.bottomAnchor, constant: -Standard.scrollViewInset).isActive = true
+      } else {
+        tempSignUpFormView.alpha = 0
+        tempSignUpFormView.heightAnchor.constraint(equalTo: visibleForms[0].heightAnchor).isActive = true
       }
     }
-  }
-  
-  private struct Standard {
-    static let space: CGFloat = 8
-    
-    static let scrollViewInset: CGFloat = 16
   }
 }
 
 extension SignUpViewController: UITextFieldDelegate {
   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-    formUpdate()
+    
     return true
   }
 }
